4b2ce92883ead8ab32b20e5032e875958cab51a3
Mohan Kandukoori <129986025+mohan-23@users.noreply.github.com>
2024-10-05 22:53:54 +0530

    Update Logest word in sentense.js

--- Logest word in sentense.js
+++ Logest word in sentense.js
@@ -1,9 +1,10 @@
 function findLogestWord(sentence) {
     const wordsList = sentence.split(' ')
-    const long = wordsList[0].length
+    const numbers = wordList.map(word => word.length)
+    const max = Math.max(...numbers)
     let longWord = ''
     for (let word of wordsList) {
-        if (word.length > long) {
+        if (word.length === max) {
             longWord = word
         }
     }
